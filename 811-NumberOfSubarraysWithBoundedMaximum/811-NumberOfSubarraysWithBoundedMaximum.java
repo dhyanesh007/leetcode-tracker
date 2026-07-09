4bef6be824969c431f432ce4ead99e48546fe5b2
dhyanesh007 <m.dhyanesh007@gmail.com>
2026-07-09 09:57:54 +0530

    Create 811-NumberOfSubarraysWithBoundedMaximum

---
+++ 811-NumberOfSubarraysWithBoundedMaximum/811-NumberOfSubarraysWithBoundedMaximum.java
@@ -0,0 +1,29 @@
+// Last updated: 7/9/2026, 9:57:54 AM
+class Solution {
+    public int numSubarrayBoundedMax(int[] nums, int left, int right) {
+        
+        int i=0;
+        int j=0;
+        int m=0;
+        int count=0;
+        
+        while(j<nums.length){
+            
+            if(nums[j] > right){
+                m=0;
+                i=j+1;
+            }
+            
+            if(nums[j]>=left && nums[j] <=right){
+                m = j-i+1;
+            }
+            count+=m;
+            j++;
+            
+            
+        }
+        
+        return count;
+        
+    }
+}
