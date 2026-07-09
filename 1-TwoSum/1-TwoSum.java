68e4ccb1226518ea93cfc21fa9ece13f431ec85f
dhyanesh007 <m.dhyanesh007@gmail.com>
2026-07-09 10:00:35 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 10:00:34 AM
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{}; 
+    }
+}
