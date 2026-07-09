29bef0c46cdf94f7870ceac8c1ee1b2ec113c214
dhyanesh007 <m.dhyanesh007@gmail.com>
2026-07-09 09:57:41 +0530

    Create 1501-CircleAndRectangleOverlapping

---
+++ 1501-CircleAndRectangleOverlapping/1501-CircleAndRectangleOverlapping.java
@@ -0,0 +1,15 @@
+// Last updated: 7/9/2026, 9:57:42 AM
+class Solution {
+    public boolean checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        
+        int close_Xaxis = Math.max(x1,Math.min(x2,xCenter));
+        int close_Yaxis = Math.max(y1,Math.min(y2,yCenter));
+
+        int distanceX = xCenter - close_Xaxis;
+        int distanceY = yCenter - close_Yaxis;
+        if(distanceX * distanceX + distanceY*distanceY <= radius*radius){
+            return true;
+        }
+        return false;
+    }
+}
