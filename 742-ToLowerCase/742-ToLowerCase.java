83ba283cfad7ebdcd4717773e70aaccfc9d8c872
dhyanesh007 <m.dhyanesh007@gmail.com>
2026-07-09 09:57:58 +0530

    Create 742-ToLowerCase

---
+++ 742-ToLowerCase/742-ToLowerCase.java
@@ -0,0 +1,11 @@
+// Last updated: 7/9/2026, 9:57:58 AM
+
+class Solution {
+    public String toLowerCase(String s) {
+        char[] a = s.toCharArray();
+        for (int i = 0; i < a.length; i++)
+            if ('A' <= a[i] && a[i] <= 'Z')
+                a[i] = (char) (a[i] - 'A' + 'a');
+        return new String(a);
+    }
+}
