c1723a5c390aa989a55b21d586f9618d54d9c3d5
dhyanesh007 <m.dhyanesh007@gmail.com>
2026-07-09 10:00:20 +0530

    Create 7-ReverseInteger

---
+++ 7-ReverseInteger/7-ReverseInteger.java
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 10:00:21 AM
+class Solution {
+    public int reverse(int x) {
+        int res = 0;
+        boolean isNegative = x < 0;
+        String strX = String.valueOf(Math.abs(x));
+        StringBuilder sb = new StringBuilder(strX).reverse();
+        
+        try {
+            res = Integer.parseInt(sb.toString());
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+        
+        return isNegative ? -res : res;       
+    }
+}
